ccfbc1fc23f4e24646ed11b77ed5de509cddc462
zaksal284 <65857234+zaksal284@users.noreply.github.com>
2023-05-30 09:37:02 +0900

    Add files via upload

---
+++ servo.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+servo_pin=23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin,GPIO.OUT)
+
+pwm=GPIO.PWM(servvo_pin,50)
+pwm.start(3.0)
+
+time.sleep(0.2)
+pwm.ChangeDutyCycle(7.5)
+
+#pwm.stop()
+#GPIO.celanup()
