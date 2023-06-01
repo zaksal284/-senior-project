fe750d63c23c9e33978d58e917d43c37854219c8
zaksal284 <65857234+zaksal284@users.noreply.github.com>
2023-06-02 04:09:11 +0900

    Update main,led.py

--- main,led.py
+++ main,led.py
@@ -4,7 +4,6 @@ import time
 import helmet
 import joystick
 import motor
-import control
 import cv2
 import deletefile
 import sys
@@ -14,116 +13,132 @@ import blackbox
 import handle
 import loadcell
 import servo
+
 time.sleep(1)
 
 # GPIO 핀 번호 설정
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 led_leftpin = 12
 led_rightpin = 6
 
 # 버튼과 LED의 핀 번호 설정
 leftbutton = 18
-
 rightbutton = 23
-
-bothbutton=24
-
-start_button=27
+bothbutton = 24
+start_button = 22
 
 detect_left = False
 detect_right = False
 detect_both = False
-start=False
+start = multiprocessing.Value("b", False)
 
+# Create and start the blackbox process, passing the shared Value as an argument
 
-
+GPIO.remove_event_detect(leftbutton)  # 기존 이벤트 감지 제거
+GPIO.remove_event_detect(bothbutton)  # 기존 이벤트 감지 제거
+GPIO.remove_event_detect(rightbutton)  # 기존 이벤트 감지 제거
+GPIO.remove_event_detect(start_button)
 # 버튼 핀의 입력 설정
 GPIO.setup(leftbutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(rightbutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(bothbutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(start_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(start_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # LED 핀의 출력 설정
 GPIO.setup(led_leftpin, GPIO.OUT)
 GPIO.setup(led_rightpin, GPIO.OUT)
 
 # 버튼 인터럽트 처리 함수
+
 def button_left_callback(channel):
     global detect_left
     detect_left = not detect_left
 
+
 def button_right_callback(channel):
     global detect_right
-    detect_right = not detect_right 
+    detect_right = not detect_right
+
 
 def button_both_callback(channel):
     global detect_both
     detect_both = not detect_both
 
+
 def button_start_callback(channel):
     global start
-    start = not start
-    
-# 이벤트 감지 등록
+    start.value = not start.value
+
+
 GPIO.add_event_detect(leftbutton, GPIO.FALLING, callback=button_left_callback, bouncetime=100)
-GPIO.add_event_detect(start_button, GPIO.FALLING, callback=button_left_callback, bouncetime=100)
+GPIO.add_event_detect(start_button, GPIO.FALLING, callback=button_start_callback, bouncetime=100)
 GPIO.add_event_detect(bothbutton, GPIO.FALLING, callback=button_both_callback, bouncetime=100)
 GPIO.add_event_detect(rightbutton, GPIO.FALLING, callback=button_right_callback, bouncetime=100)
 
 
-
 while True:
-    if start:
+    if start.value:
         break
+    print("시작버튼")
+    time.sleep(0.5)
     pass
 
-helmet.ReadVol(0)
+helmet.ReadVol(1)
 
 loadcell.read_serial_data()
 
-servo.serct()
+servo.serct2()
 
-def main():
-    global detect_right
+time.sleep(1)
+
+
+def main(start):
     global detect_left
-    multi=multiprocessing.Process(target=blackbox.cctv)
+    global detect_both
+    global detect_right
+    multi = multiprocessing.Process(target=blackbox.cctv, args=(start,))
+    multi.start()
+    multi = multiprocessing.Process(target=blackbox.cctv)
     multi.start()
     while True:
         motor.motor_control()
-        if detect_both == True:
+        if detect_both:
             GPIO.output(led_leftpin, GPIO.HIGH)
             GPIO.output(led_rightpin, GPIO.HIGH)
             time.sleep(0.5)
             GPIO.output(led_leftpin, GPIO.LOW)
             GPIO.output(led_rightpin, GPIO.LOW)
-            time.sleep(0.5) 
-            
-        if detect_left == True:
+            print("비상")
+            time.sleep(0.5)
+
+        if detect_left:
             GPIO.output(led_leftpin, GPIO.HIGH)
             time.sleep(0.5)
             GPIO.output(led_leftpin, GPIO.LOW)
-            if handle.read_adc(3)<380:  #값변경 필요
-                detect_left= not detect_left
             time.sleep(0.5)
- 
-        if detect_right == True:
+            print("왼")
+            if handle.read_adc(3) > 650:  # 값변경 필요
+                detect_left = not detect_left
+
+        if detect_right:
             GPIO.output(led_rightpin, GPIO.HIGH)
             time.sleep(0.5)
             GPIO.output(led_rightpin, GPIO.LOW)
             time.sleep(0.5)
-            if handle.read_adc(3)>480:   #값변경 필요
-                detect_right= not detect_right
-        if start_button == False:
-             try:
-                 motor.pwm1.stop()
-                 motor.pwm2.stop()
-                 GPIO.clean()   
-                 multi.join(10)
-                 sys.exit()
-             except TimeoutError:
-                 multi.terminate() 
-                 sys.exit()
+            print("오")
+            if handle.read_adc(3) < 410:  # 값변경 필요
+                detect_right = not detect_right
+
+        if not start.value:
+            motor.pwm1.stop()
+            motor.pwm2.stop()
+            servo.serct()
+            GPIO.cleanup()
+            time.sleep(1.0)
+            multi.join()
+            sys.exit()
 
 
 if __name__ == "__main__":
-    main()
+    main(start)
